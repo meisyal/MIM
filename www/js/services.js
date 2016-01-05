ab3771718761c3624f2da197d9abf0ea4c32fdea
Andrias Meisyal <andriasmy@live.com>
2016-01-05 20:37:04 +0700

    Add global query function getAll and getById

--- www/js/services.js
+++ www/js/services.js
@@ -16,4 +16,28 @@ MIM.factory('DB', function($q, $ionicPlatform, $cordovaSQLite) {
 
     return q.promise;
   };
+
+  this.getAll = function(res) {
+    var output = [];
+
+    if (res.rows.length) {
+      for (var i = 0; i < res.rows.length; i++) {
+        output.push(res.rows.item(i));
+      }
+    }
+
+    return output;
+  };
+
+  this.getById = function(res) {
+    var output = null;
+
+    if (res.rows.length) {
+      angular.copy(output, res.rows.item(0));
+    }
+
+    return output;
+  }
+
+  return this;
 });
